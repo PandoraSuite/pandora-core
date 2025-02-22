3943b547a07d2d79f87d6a1c003cc09329b8554f
Mateo Aguirre <zaguirrez05@gmail.com>
2025-02-22 00:06:32 -0500

    add connection to the database

--- internal/adapters/persistence/db.go
+++ internal/adapters/persistence/db.go
@@ -1,1 +1,31 @@
 package persistence
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type Database struct {
+	pool *pgxpool.Pool
+}
+
+func (p *Database) Close() { p.pool.Close() }
+
+func NewDatabase(dns string) (*Database, error) {
+	config, err := pgxpool.ParseConfig(dns)
+	if err != nil {
+		return nil, fmt.Errorf("error al parsear configuración de la BD: %w", err)
+	}
+
+	config.HealthCheckPeriod = 1 * time.Minute
+
+	pool, err := pgxpool.NewWithConfig(context.Background(), config)
+	if err != nil {
+		return nil, fmt.Errorf("error al crear el pool de conexiones: %w", err)
+	}
+
+	return &Database{pool: pool}, nil
+}
