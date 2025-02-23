79d2d526a89e1380353eb2c65f85e8becec0633a
Mateo Aguirre <zaguirrez05@gmail.com>
2025-02-23 17:27:46 -0500

    add the use case for the creation of an project

--- internal/app/project.go
+++ internal/app/project.go
@@ -1,1 +1,42 @@
 package app
+
+import (
+	"context"
+	"errors"
+
+	"github.com/MAD-py/pandora-core/internal/domain/dto"
+	"github.com/MAD-py/pandora-core/internal/domain/entities"
+	"github.com/MAD-py/pandora-core/internal/ports/outbound"
+)
+
+type ProjectUseCase struct {
+	clientRepo outbound.ProjectRepositoryPort
+}
+
+func (c *ProjectUseCase) Create(
+	ctx context.Context, req *dto.ProjectCreate,
+) (*dto.ProjectResponse, error) {
+	if req.Name == "" {
+		return nil, errors.New("name of the project cannot be empty")
+	}
+
+	client, err := c.clientRepo.Save(
+		ctx,
+		&entities.Project{
+			Name:     req.Name,
+			Status:   req.Status,
+			ClientID: req.ClientID,
+		},
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return &dto.ProjectResponse{
+		ID:        client.ID,
+		Name:      client.Name,
+		Status:    client.Status,
+		ClientID:  client.ClientID,
+		CreatedAt: client.CreatedAt,
+	}, nil
+}
