778ee3fdc1d6c52702e6a4e34239a9fd1b64772b
Mateo Aguirre <zaguirrez05@gmail.com>
2025-03-08 21:22:21 -0500

    cosmetics

--- cmd/http/main.go
+++ cmd/http/main.go
@@ -15,37 +15,31 @@ import (
 func main() {
 	log.Println("[INFO] Starting Pandora Core...")
 
-	AppConfig, err := config.LoadConfig()
+	cfg, err := config.LoadConfig()
 	if err != nil {
-		panic(err)
+		log.Fatalf("[ERROR] Error loading configuration: %v", err)
 	}
 
 	log.Println("[INFO] Configuration loaded successfully from environment variables and configuration files.")
 
-	db, err := persistence.NewPersistence(AppConfig.DBDNS())
+	db, err := persistence.NewPersistence(cfg.DBDNS())
 	if err != nil {
-		panic(err)
+		log.Fatalf("[ERROR] Failed to initialize persistence: %v", err)
 	}
+	pool := db.Pool()
+	errHandler := db.HandlerErr()
 
-	apiKeyRepo := repository.NewAPIKeyRepository(db.Pool(), db.HandlerErr())
-	clientRepo := repository.NewClientRepository(db.Pool(), db.HandlerErr())
-	serviceRepo := repository.NewServiceRepository(db.Pool(), db.HandlerErr())
-	projectRepo := repository.NewProjectRepository(db.Pool(), db.HandlerErr())
-	jwtProvider := security.NewJWTProvider([]byte(AppConfig.JWTSecret()))
-	requestLogRepo := repository.NewRequestLogRepository(
-		db.Pool(), db.HandlerErr(),
-	)
-	environmentRepo := repository.NewEnvironmentRepository(
-		db.Pool(), db.HandlerErr(),
-	)
-	projectServiceRepo := repository.NewProjectServiceRepository(
-		db.Pool(), db.HandlerErr(),
-	)
-	environmentServiceRepo := repository.NewEnvironmentServiceRepository(
-		db.Pool(), db.HandlerErr(),
-	)
+	apiKeyRepo := repository.NewAPIKeyRepository(pool, errHandler)
+	clientRepo := repository.NewClientRepository(pool, errHandler)
+	serviceRepo := repository.NewServiceRepository(pool, errHandler)
+	projectRepo := repository.NewProjectRepository(pool, errHandler)
+	requestLogRepo := repository.NewRequestLogRepository(pool, errHandler)
+	environmentRepo := repository.NewEnvironmentRepository(pool, errHandler)
+	projectServiceRepo := repository.NewProjectServiceRepository(pool, errHandler)
+	environmentServiceRepo := repository.NewEnvironmentServiceRepository(pool, errHandler)
 
-	credentialsFile, err := AppConfig.CredentialsFile()
+	jwtProvider := security.NewJWTProvider([]byte(cfg.JWTSecret()))
+	credentialsFile, err := cfg.CredentialsFile()
 	if err != nil {
 		panic(err)
 	}
@@ -59,15 +53,11 @@ func main() {
 	clientUseCase := app.NewClientUseCase(clientRepo)
 	serviceUseCase := app.NewServiceUseCase(serviceRepo)
 	projectUseCase := app.NewProjectUseCase(projectRepo, projectServiceRepo)
-	environmentUseCase := app.NewEnvironmentUseCase(
-		environmentRepo, projectServiceRepo, environmentServiceRepo,
-	)
-	apiKeyUseCase := app.NewAPIKeyUseCase(
-		apiKeyRepo, requestLogRepo, serviceRepo, environmentServiceRepo,
-	)
+	environmentUseCase := app.NewEnvironmentUseCase(environmentRepo, projectServiceRepo, environmentServiceRepo)
+	apiKeyUseCase := app.NewAPIKeyUseCase(apiKeyRepo, requestLogRepo, serviceRepo, environmentServiceRepo)
 
 	srv := http.NewServer(
-		fmt.Sprintf(":%s", AppConfig.HTTPPort()),
+		fmt.Sprintf(":%s", cfg.HTTPPort()),
 		serviceUseCase,
 		authUseCase,
 		apiKeyUseCase,
