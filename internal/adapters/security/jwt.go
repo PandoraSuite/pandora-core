f61e1fdf330c1df84e2feb87a968fb3aeeefc5c4
Mateo Aguirre <zaguirrez05@gmail.com>
2025-04-16 20:54:10 -0500

    fix: set global time zone to UTC

--- internal/adapters/security/jwt.go
+++ internal/adapters/security/jwt.go
@@ -17,7 +17,7 @@ type JWTProvider struct {
 func (p *JWTProvider) GenerateToken(
 	ctx context.Context, subject string,
 ) (*dto.TokenResponse, *errors.Error) {
-	now := time.Now().UTC()
+	now := time.Now()
 	expTime := now.Add(time.Hour)
 
 	claims := jwt.MapClaims{
