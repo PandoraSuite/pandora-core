b638c82cf863286266440b05a8da7e2ed81c66cc
Mateo Aguirre <zaguirrez05@gmail.com>
2025-05-05 23:34:08 -0500

    refactor: Update DTOs to use 'name' tags for improved validation and consistency

--- internal/domain/dto/reservation.go
+++ internal/domain/dto/reservation.go
@@ -3,14 +3,14 @@ package dto
 import "github.com/MAD-py/pandora-core/internal/domain/enums"
 
 type ReservationWithDetails struct {
-	ID                string                  `json:"id"`
-	StartRequestID    string                  `json:"start_request_id"`
-	APIKey            string                  `json:"api_key"`
-	ServiceID         int                     `json:"service_id"`
-	ServiceName       string                  `json:"service_name"`
-	ServiceVersion    string                  `json:"service_version"`
-	ServiceStatus     enums.ServiceStatus     `json:"service_status"`
-	EnvironmentID     int                     `json:"environment_id"`
-	EnvironmentName   string                  `json:"environment_name"`
-	EnvironmentStatus enums.EnvironmentStatus `json:"environment_status"`
+	ID                string                  `name:"id"`
+	StartRequestID    string                  `name:"start_request_id"`
+	APIKey            string                  `name:"api_key"`
+	ServiceID         int                     `name:"service_id"`
+	ServiceName       string                  `name:"service_name"`
+	ServiceVersion    string                  `name:"service_version"`
+	ServiceStatus     enums.ServiceStatus     `name:"service_status"`
+	EnvironmentID     int                     `name:"environment_id"`
+	EnvironmentName   string                  `name:"environment_name"`
+	EnvironmentStatus enums.EnvironmentStatus `name:"environment_status"`
 }
