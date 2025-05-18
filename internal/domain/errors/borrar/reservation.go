7368e55e74f8a6eeab6d8038923283d9d3989524
Mateo Aguirre <zaguirrez05@gmail.com>
2025-05-17 23:45:32 -0500

    refactor(errors): Delete obsolete error files in the delete module

--- internal/domain/errors/borrar/reservation.go
+++
@@ -1,6 +0,0 @@
-package errors
-
-// var (
-// 	ErrReservationNotFound         = NewError(CodeNotFound, "Reservation not found")
-// 	ErrReservationGenerationFailed = NewError(CodeInternalError, "Reservation generation failed")
-// )
