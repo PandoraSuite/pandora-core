0cf85ff8305764d3c0a6616b54cd8452e7fdddd2
Mateo Aguirre <zaguirrez05@gmail.com>
2025-05-17 23:42:47 -0500

    bug(persistence): remove unnecessary sync.Once usage in postgresRepositories

--- internal/adapters/persistence/postgres.go
+++ internal/adapters/persistence/postgres.go
@@ -1,16 +1,12 @@
 package persistence
 
 import (
-	"sync"
-
 	"github.com/MAD-py/pandora-core/internal/adapters/persistence/postgres"
 	"github.com/MAD-py/pandora-core/internal/ports"
 )
 
 type postgresRepositories struct {
 	driver *postgres.Driver
-
-	once sync.Once
 
 	apiKeyRepo      ports.APIKeyRepository
 	clientRepo      ports.ClientRepository
@@ -24,50 +20,50 @@ type postgresRepositories struct {
 func (r *postgresRepositories) Close() { r.driver.Close() }
 
 func (r *postgresRepositories) APIKey() ports.APIKeyRepository {
-	r.once.Do(func() {
+	if r.apiKeyRepo == nil {
 		r.apiKeyRepo = postgres.NewAPIKeyRepository(r.driver)
-	})
+	}
 	return r.apiKeyRepo
 }
 
 func (r *postgresRepositories) Client() ports.ClientRepository {
-	r.once.Do(func() {
+	if r.clientRepo == nil {
 		r.clientRepo = postgres.NewClientRepository(r.driver)
-	})
+	}
 	return r.clientRepo
 }
 
 func (r *postgresRepositories) Project() ports.ProjectRepository {
-	r.once.Do(func() {
+	if r.projectRepo == nil {
 		r.projectRepo = postgres.NewProjectRepository(r.driver)
-	})
+	}
 	return r.projectRepo
 }
 
 func (r *postgresRepositories) Service() ports.ServiceRepository {
-	r.once.Do(func() {
+	if r.serviceRepo == nil {
 		r.serviceRepo = postgres.NewServiceRepository(r.driver)
-	})
+	}
 	return r.serviceRepo
 }
 
 func (r *postgresRepositories) Request() ports.RequestRepository {
-	r.once.Do(func() {
+	if r.requestRepo == nil {
 		r.requestRepo = postgres.NewRequestLogRepository(r.driver)
-	})
+	}
 	return r.requestRepo
 }
 
 func (r *postgresRepositories) Environment() ports.EnvironmentRepository {
-	r.once.Do(func() {
+	if r.environmentRepo == nil {
 		r.environmentRepo = postgres.NewEnvironmentRepository(r.driver)
-	})
+	}
 	return r.environmentRepo
 }
 
 func (r *postgresRepositories) Reservation() ports.ReservationRepository {
-	r.once.Do(func() {
+	if r.reservationRepo == nil {
 		r.reservationRepo = postgres.NewReservationRepository(r.driver)
-	})
+	}
 	return r.reservationRepo
 }
