0ccdf7c7bfee62d40dd38de2b24ea5b0f08fcfc0
Mateo Aguirre <zaguirrez05@gmail.com>
2025-06-06 21:53:53 -0500

    feat(test): Add unit tests and mock for Delete use case in ProjectRepository

---
+++ internal/app/project/delete/use_case_test.go
@@ -0,0 +1,109 @@
+package delete
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/suite"
+	"go.uber.org/mock/gomock"
+
+	"github.com/MAD-py/pandora-core/internal/app/project/delete/mock"
+	"github.com/MAD-py/pandora-core/internal/domain/errors"
+	mockvalidator "github.com/MAD-py/pandora-core/internal/validator/mock"
+)
+
+type Suite struct {
+	suite.Suite
+
+	ctrl *gomock.Controller
+
+	validator   *mockvalidator.MockValidator
+	projectRepo *mock.MockProjectRepository
+
+	useCase UseCase
+
+	ctx context.Context
+}
+
+func (s *Suite) SetupTest() {
+	time.Local = time.UTC
+
+	s.ctrl = gomock.NewController(s.T())
+
+	s.projectRepo = mock.NewMockProjectRepository(s.ctrl)
+	s.validator = mockvalidator.NewMockValidator(s.ctrl)
+
+	s.useCase = NewUseCase(s.validator, s.projectRepo)
+
+	s.ctx = context.Background()
+}
+
+func (s *Suite) TearDownTest() {
+	s.ctrl.Finish()
+}
+
+func (s *Suite) TestSuccess() {
+	id := 42
+
+	s.validator.EXPECT().
+		ValidateVariable(id, "id", gomock.Any(), gomock.Any()).
+		Return(nil).
+		Times(1)
+
+	s.projectRepo.EXPECT().
+		Delete(s.ctx, id).
+		Return(nil).
+		Times(1)
+
+	err := s.useCase.Execute(s.ctx, id)
+
+	s.Require().NoError(err)
+}
+
+func (s *Suite) TestValidationError() {
+	id := -1
+
+	validationErr := errors.NewValidationFailed("Validation Error", nil)
+	s.validator.EXPECT().
+		ValidateVariable(id, "id", gomock.Any(), gomock.Any()).
+		Return(validationErr).
+		Times(1)
+
+	s.projectRepo.EXPECT().
+		Delete(s.ctx, id).
+		Times(0)
+
+	err := s.useCase.Execute(s.ctx, id)
+
+	s.Require().Error(err)
+
+	s.Equal(errors.CodeValidationFailed, err.Code())
+	s.Equal(validationErr, err)
+}
+
+func (s *Suite) TestRepositoryError() {
+	id := 42
+
+	s.validator.EXPECT().
+		ValidateVariable(id, "id", gomock.Any(), gomock.Any()).
+		Return(nil).
+		Times(1)
+
+	repositoryErr := errors.NewInternal("Repository Error", nil)
+	s.projectRepo.EXPECT().
+		Delete(s.ctx, id).
+		Return(repositoryErr).
+		Times(1)
+
+	err := s.useCase.Execute(s.ctx, id)
+
+	s.Require().Error(err)
+
+	s.Equal(errors.CodeInternal, err.Code())
+	s.Equal(repositoryErr, err)
+}
+
+func TestServiceCreateSuite(t *testing.T) {
+	suite.Run(t, new(Suite))
+}
